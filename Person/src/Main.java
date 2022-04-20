d1e9619fb0de19bf17b2c27b07956edbe440d6df
agubeydullin@gmail.com <agubeydullin@gmail.com>
2022-04-21 00:14:10 +0300

    Работает?

---
+++ Person/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    String fullName;
+    int age;
+
+    public static void main(String[] args) {
+        Person person = new Person("Brown");
+        Person person1 = new Person(25);
+        person.talk();
+        person.move();
+
+
+
+    }
+}
---
+++ Person/src/Person.java
@@ -0,0 +1,35 @@
+public class Person {
+
+    String fullName;
+    int age;
+
+    public Person(String fullName) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public Person(int age) {
+        this.age = 25;
+        this.fullName = "Brown";
+    }
+
+    public String move() {
+        System.out.println("Такой то Персон куда то идет");
+    return fullName;
+    }
+
+    public String talk() {
+        System.out.println("Такой то Персон чего то говорит");
+    return  fullName;
+    }
+
+}
+
+
+
+
+
+
+
+
+
